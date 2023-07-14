531f9b25bccd1900a10a3a0b46eae58ff3e9e0cf
Paweł Sienkiewicz <sienkol2001@gmail.com>
2023-07-14 23:45:56 +0200

    Szkielet dodawania rezerwacji

--- materialy sql/BAZA.sql
+++ materialy sql/BAZA.sql
@@ -1,10 +1,2 @@
-
-CREATE TABLE reservations(
-reservation_number INT NOT NULL,
-room_number INT NOT NULL,
-guest_id INT NOT NULL,
-arrival_date DATE NOT NULL,
-departure_date DATE NOT NULL,
-paid ENUM('YES', 'NO') NOT NULL,
-PRIMARY KEY (reservation_number)
-);
+INSERT INTO rooms(room_number, floor, guests_capacity)
+VALUES (202, 2, 3)
