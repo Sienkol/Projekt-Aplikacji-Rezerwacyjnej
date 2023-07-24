6aa48a350aaba84edfdcd2387aa77f6d4b68f860
Paweł Sienkiewicz <sienkol2001@gmail.com>
2023-07-24 23:37:41 +0200

    Naprawione dodawanie do baz danych/Zamieniono MYSQL na SQLAlchemy

--- app.py
+++ app.py
@@ -1,14 +1,45 @@
 from flask import Flask, render_template, request, url_for
-from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:6133@127.0.0.1/rooms"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
-app.config["MYSQL_HOST"] = "127.0.0.1"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "6133"
-app.config["MYSQL_DB"] = "rooms"
-mysql = MySQL(app)
+
+class rooms(db.Model):
+    room_number = db.Column(db.Integer, primary_key=True)
+    floor = db.Column(db.Integer, nullable=False)
+    guests_capacity = db.Column(db.Integer, nullable=False)
+
+
+class previous_reservations(db.Model):
+    reservation_number = db.Column(db.Integer, primary_key=True)
+    room_number = db.Column(db.Integer, nullable=False)
+    guest_id = db.Column(db.Integer, nullable=False)
+    arrival_date = db.Column(db.Date, nullable=False)
+    departure_date = db.Column(db.Date, nullable=False)
+    completed = db.Column(db.String(50), nullable=False)
+
+
+class guests(db.Model):
+    guest_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    number_of_visits = db.Column(db.Integer, nullable=False)
+    birth_date = db.Column(db.Date, nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+
+
+class reservations(db.Model):
+    reservation_number = db.Column(db.Integer, primary_key=True)
+    room_number = db.Column(db.Integer, nullable=False)
+    guest_id = db.Column(db.Integer, nullable=False)
+    arrival_date = db.Column(db.Date, nullable=False)
+    departure_date = db.Column(db.Date, nullable=False)
+    paid = db.Column(db.String(50), nullable=False)
 
 
 ##Strona główna
@@ -31,17 +62,13 @@ def tabele():
 
 @app.route("/BazaPokoi")
 def baza_pokoi():
-    cursor = mysql.connect.cursor()
-    cursor.execute("SELECT * FROM rooms")
-    baza_pokoi = cursor.fetchall()
+    baza_pokoi = rooms.query.all()
     return render_template("baza_pokoi.html", baza_pokoi=baza_pokoi)
 
 
 @app.route("/ArchiwumRezerwacji")
 def archiwum_rezerwacji():
-    cursor = mysql.connect.cursor()
-    cursor.execute("SELECT * FROM previous_reservations")
-    archiwum_rezerwacji = cursor.fetchall()
+    archiwum_rezerwacji = previous_reservations.query.all()
     return render_template(
         "archiwum_rezerwacji.html", archiwum_rezerwacji=archiwum_rezerwacji
     )
@@ -49,17 +76,13 @@ def archiwum_rezerwacji():
 
 @app.route("/BazaGosci")
 def baza_gosci():
-    cursor = mysql.connect.cursor()
-    cursor.execute("SELECT * FROM guests")
-    baza_gosci = cursor.fetchall()
+    baza_gosci = guests.query.all()
     return render_template("baza_gosci.html", baza_gosci=baza_gosci)
 
 
 @app.route("/ObecneRezerwacje")
 def obecne_rezerwacje():
-    cursor = mysql.connect.cursor()
-    cursor.execute("SELECT * FROM reservations")
-    obecne_rezerwacje = cursor.fetchall()
+    obecne_rezerwacje = reservations.query.all()
     return render_template(
         "obecne_rezerwacje.html", obecne_rezerwacje=obecne_rezerwacje
     )
@@ -79,13 +102,17 @@ def sikuel():
         departure_date = request.form["departure_date"]
         email = request.form["email"]
         room_number = request.form["room_number"]
-        cursor = mysql.connect.cursor()
-        cursor.execute(
-            """INSERT INTO guests (first_name, last_name, number_of_visits, birth_date, email) VALUES (%s, %s, 1, %s, %s)""",
-            (name, surname, birth_date, email),
+
+        # Create and add the new Guest to the database
+        new_guest = guests(
+            first_name=name,
+            last_name=surname,
+            birth_date=birth_date,
+            email=email,
         )
-        mysql.connection.commit()
-        cursor.close()
+        db.session.add(new_guest)
+        db.session.commit()
+
     return render_template("orders.html")
 
 
